0cf9fcd1ec951ab7b75e0b7c20f62541895f11de
Sergei Krasnoshapka <1324galaxy@gmail.com>
2024-01-15 18:51:17 +1000

    added second task

--- Program.cs
+++ Program.cs
@@ -12,17 +12,26 @@
 // int result = number * number; // 81
 
 // Console.ReadLine() - получить информацию из строчки
-Console.Write("Введите первое число: ");
-int firstNumber = Convert.ToInt32(Console.ReadLine());
+/* 
+ */
 
-Console.Write("Введите первое число: ");
-int secondNumber = Convert.ToInt32(Console.ReadLine());
-// f = 25; s = 5: f == s*s, 25 == 5 * 5
-if(firstNumber == secondNumber * secondNumber) // a является квадратом от числа b
+// On Windows: Shift + Alt + F
+// On Mac: Shift + Option + F
+
+// Напишите программу, которая на вход принимает
+// целое число N, а на выходе показывает все целые
+// числа в промежутке от -N до N.
+// Примеры
+// 4 => -4, -3, -2, -1, 0, 1, 2, 3, 4
+// 2 => -2, -1, 0, 1, 2
+
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+// N = 4,
+int negativeN = N * -1; // -4
+
+while (negativeN <= N)// -3 -2 -1 0 1 2 3
 {
-    Console.WriteLine("Да, " + firstNumber + " является кв. от числа " + secondNumber);
-}
-else // a не является кв от числа b 
-{
-    Console.WriteLine("НЕТ, " + firstNumber + " НЕ является кв. от числа " + secondNumber);
+    Console.Write(negativeN + " ");
+    negativeN++; // negativeN++ (negativeN = negativeN + 1)
 }
--- obj/Debug/net8.0/sem_1.AssemblyInfo.cs
+++ obj/Debug/net8.0/sem_1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sem_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11ab47fbbe7d9a58bfa385cd0d4e53852d3d8426")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a17a3a8b3a2f17982a00abdf99f2a71b3fc2f1b")]
 [assembly: System.Reflection.AssemblyProductAttribute("sem_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sem_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
